d0d88236afda5a45dcf74acdc1ca9aac39db8177
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-01-26 19:10:59 -0500

    done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
 import { useState } from "react";
 import {
 	useFonts,
