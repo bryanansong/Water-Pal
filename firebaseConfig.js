14199a684c0ad51a1fe2f3097b43c469df5a1dac
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-01-28 19:21:55 -0500

    Added firebase configuration

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,20 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
-
-// Initialize Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "api-key",
-	authDomain: "project-id.firebaseapp.com",
-	databaseURL: "https://project-id.firebaseio.com",
-	projectId: "project-id",
-	storageBucket: "project-id.appspot.com",
-	messagingSenderId: "sender-id",
-	appId: "app-id",
-	measurementId: "G-measurement-id",
+	apiKey: "AIzaSyC11HCWYnvHwebeE7h9DWYKiO-hMnHT7PQ",
+	authDomain: "water-pal-app.firebaseapp.com",
+	projectId: "water-pal-app",
+	storageBucket: "water-pal-app.appspot.com",
+	messagingSenderId: "35726380964",
+	appId: "1:35726380964:web:67427947d6acc751e6bb31",
+	measurementId: "G-5VFZNDE4YY",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+const analytics = getAnalytics(app);
