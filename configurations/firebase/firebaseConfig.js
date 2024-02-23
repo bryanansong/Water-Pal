4141afe577658f40136a94f705fdd6b856e8635a
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-02-23 17:19:29 -0500

    imported authentication

--- configurations/firebase/firebaseConfig.js
+++ configurations/firebase/firebaseConfig.js
@@ -1,11 +1,5 @@
 import { initializeApp } from "firebase/app";
-
-// Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 // Initialize Firebase
 const firebaseConfig = {
@@ -19,6 +13,5 @@ const firebaseConfig = {
 	measurementId: "G-5VFZNDE4YY",
 };
 
-const app = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
