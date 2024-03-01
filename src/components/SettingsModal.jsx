17caa1d4a6e6a1756000851d45a6cdf114569997
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-01 18:02:40 -0500

    Created settings modal component

---
+++ src/components/SettingsModal.jsx
@@ -0,0 +1,13 @@
+import { View, Text, Modal } from "react-native";
+
+const SettingsModal = ({}) => {
+	return (
+		<Modal>
+			<View>
+				<Text>Settings Modal</Text>
+			</View>
+		</Modal>
+	);
+};
+
+export default SettingsModal;
