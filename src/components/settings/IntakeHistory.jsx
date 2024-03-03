b9c4d7ced2a64ff0a34b86a6990778432cda0389
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-03 03:18:29 -0500

    Added a placeholder screen to the History modal

--- src/components/settings/IntakeHistory.jsx
+++ src/components/settings/IntakeHistory.jsx
@@ -4,13 +4,20 @@ import { Button, Text, View } from "react-native";
 const IntakeHistory = ({ setIntakeHistoryModalOpen }) => {
 	return (
 		<View className="bg-white w-full p-4 rounded-xl">
-			<Text>Take a look at your history</Text>
-			<Button
-				title="Close"
-				onPress={() => {
-					setIntakeHistoryModalOpen(false);
-				}}
-			></Button>
+			<View className="flex-row justify-between">
+				<Text className="text-xl font-semibold">Your history</Text>
+				<Button
+					title="X"
+					onPress={() => {
+						setIntakeHistoryModalOpen(false);
+					}}
+				></Button>
+			</View>
+			<View className=" max-w-md">
+				<Text className="self-center text-3xl m-5 tracking-widest">
+					Coming Soon!
+				</Text>
+			</View>
 		</View>
 	);
 };
