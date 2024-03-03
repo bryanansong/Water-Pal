33d7594ab43e30adf1cd97a36df816931dac3cd3
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-03 02:43:54 -0500

    Fixed bug with reminder modal not closing properly

--- src/components/settings/Reminder.jsx
+++ src/components/settings/Reminder.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
 
-const Reminder = ({ setReminderModalOpen }) => {
+const Reminder = ({ setRemindersModalOpen }) => {
 	return (
 		<View className="bg-white w-full p-4 rounded-xl">
 			<Text>Set your Reminder</Text>
 			<Button
 				title="Close"
 				onPress={() => {
-					setReminderModalOpen(false);
+					setRemindersModalOpen(false);
 				}}
 			></Button>
 		</View>
