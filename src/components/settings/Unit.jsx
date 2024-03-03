2738b863449a6b7366da202242880d810d63059c
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-03 02:44:19 -0500

    Fixed bug for Unit modal not closing properly

--- src/components/settings/Unit.jsx
+++ src/components/settings/Unit.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button, Text, View } from "react-native";
 
-const Unit = ({ setUnitModalOpen }) => {
+const Unit = ({ setUnitsModalOpen }) => {
 	return (
 		<View className="bg-white w-full p-4 rounded-xl">
 			<Text>Change your Unit</Text>
 			<Button
 				title="Close"
 				onPress={() => {
-					setUnitModalOpen(false);
+					setUnitsModalOpen(false);
 				}}
 			></Button>
 		</View>
