392c405f8dbdb9cd0f56168ada7814d2c9ddcbff
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-03 03:16:52 -0500

    Added a placeholder screen to the Daily Goal modal

--- src/components/settings/DailyGoal.jsx
+++ src/components/settings/DailyGoal.jsx
@@ -4,13 +4,20 @@ import { Button, Text, View } from "react-native";
 const DailyGoal = ({ setDailyGoalModalOpen }) => {
 	return (
 		<View className="bg-white w-full p-4 rounded-xl">
-			<Text>Set your daily goal</Text>
-			<Button
-				title="Close"
-				onPress={() => {
-					setDailyGoalModalOpen(false);
-				}}
-			></Button>
+			<View className="flex-row justify-between">
+				<Text className="text-xl font-semibold">Set your daily goal</Text>
+				<Button
+					title="X"
+					onPress={() => {
+						setDailyGoalModalOpen(false);
+					}}
+				></Button>
+			</View>
+			<View className=" max-w-md">
+				<Text className="self-center text-3xl m-5 tracking-widest">
+					Change Goal Here
+				</Text>
+			</View>
 		</View>
 	);
 };
