e6cc199f61410ea2578143bce9f31ad283b23e58
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-03 03:21:35 -0500

    Added a placeholder screen to the Feedback and Notifications Preferecences modals

--- src/components/settings/Feedback.jsx
+++ src/components/settings/Feedback.jsx
@@ -4,13 +4,22 @@ import { Button, Text, View } from "react-native";
 const Feedback = ({ setFeedbackModalOpen }) => {
 	return (
 		<View className="bg-white w-full p-4 rounded-xl">
-			<Text>Send Feedback/Inquiries</Text>
-			<Button
-				title="Close"
-				onPress={() => {
-					setFeedbackModalOpen(false);
-				}}
-			></Button>
+			<View className="flex-row justify-between">
+				<Text className="text-xl font-semibold">
+					Send Feedback/Inquiries
+				</Text>
+				<Button
+					title="X"
+					onPress={() => {
+						setFeedbackModalOpen(false);
+					}}
+				></Button>
+			</View>
+			<View className=" max-w-md">
+				<Text className="self-center text-3xl m-5 tracking-widest">
+					Coming Soon!
+				</Text>
+			</View>
 		</View>
 	);
 };
