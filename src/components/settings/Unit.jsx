29c62d53ba4bbeb69d679c547a0048a6b27acd07
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-03 03:01:49 -0500

    Added a coming soon screen to the Unit modal

--- src/components/settings/Unit.jsx
+++ src/components/settings/Unit.jsx
@@ -4,13 +4,20 @@ import { Button, Text, View } from "react-native";
 const Unit = ({ setUnitsModalOpen }) => {
 	return (
 		<View className="bg-white w-full p-4 rounded-xl">
-			<Text>Change your Unit</Text>
-			<Button
-				title="Close"
-				onPress={() => {
-					setUnitsModalOpen(false);
-				}}
-			></Button>
+			<View className="flex-row justify-between">
+				<Text className="text-xl font-semibold">Change your Unit</Text>
+				<Button
+					title="X"
+					onPress={() => {
+						setUnitsModalOpen(false);
+					}}
+				></Button>
+			</View>
+			<View className=" max-w-md">
+				<Text className="self-center text-3xl m-5 tracking-widest">
+					Coming Soon!
+				</Text>
+			</View>
 		</View>
 	);
 };
