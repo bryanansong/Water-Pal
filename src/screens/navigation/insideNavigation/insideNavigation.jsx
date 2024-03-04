ba50fd7bf8f0061652cb8af5e3043aa517ecb84a
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-04 10:57:46 -0500

    Added Placeholder screen to the navigation

--- src/screens/navigation/insideNavigation/insideNavigation.jsx
+++ src/screens/navigation/insideNavigation/insideNavigation.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../../home/home";
 import Settings from "../../settings/Settings";
+import Profile from "../../profile/Profile";
+import ProfilePlaceHolder from "../../profile/ProfilePlaceHolder";
 
 const InsideNavigation = () => {
 	const Stack = createNativeStackNavigator();
@@ -15,6 +17,23 @@ const InsideNavigation = () => {
 			<Stack.Screen
 				name="Settings"
 				component={Settings}
+				options={{
+					headerBlurEffect: "systemUltraThinMaterialLight",
+					headerTransparent: true,
+				}}
+			/>
+			<Stack.Screen
+				name="Profile"
+				component={Profile}
+				options={{
+					headerBlurEffect: "systemUltraThinMaterialLight",
+					headerTransparent: true,
+				}}
+			/>
+			{/** TODO: Delete the placeholder Screen when ready */}
+			<Stack.Screen
+				name="Profile_placeholder"
+				component={ProfilePlaceHolder}
 				options={{
 					headerBlurEffect: "systemUltraThinMaterialLight",
 					headerTransparent: true,
