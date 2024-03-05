918164e9981fdaecc59ee5ec7ca0faef0255465d
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-03-05 01:42:52 -0500

    Added functionality to display history data

--- src/components/settings/IntakeHistory.jsx
+++ src/components/settings/IntakeHistory.jsx
@@ -1,7 +1,35 @@
-import React from "react";
-import { Button, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Button, Text, View } from "react-native";
+import { ScrollView } from "react-native";
+import { Table, Row } from "react-native-reanimated-table";
 
-const IntakeHistory = ({ setIntakeHistoryModalOpen }) => {
+const IntakeHistory = ({ setIntakeHistoryModalOpen, userHistory }) => {
+	const [tableHead] = useState(["Date", "Goal", "Progress", "Percentage"]);
+	const [loading, setLoading] = useState(true);
+	const [intakeHistory, setIntakeHistory] = useState([]);
+
+	const convertDataToArray = (inputData) => {
+		const result = Object.entries(inputData)
+			.map(([date, { goal, progress }]) => {
+				const percentage = goal > 0 ? (progress / goal) * 100 : 0;
+				return [
+					date,
+					goal.toLocaleString(),
+					progress.toLocaleString(),
+					`${Math.round(percentage)}%`,
+				];
+			})
+			.sort((a, b) => new Date(b[0]) - new Date(a[0]));
+
+		return result;
+	};
+
+	useEffect(() => {
+		const formattedResult = convertDataToArray(userHistory);
+		setIntakeHistory(formattedResult);
+		setLoading(false);
+	}, [userHistory]);
+
 	return (
 		<View className="bg-white w-full p-4 rounded-xl">
 			<View className="flex-row justify-between">
@@ -13,11 +41,43 @@ const IntakeHistory = ({ setIntakeHistoryModalOpen }) => {
 					}}
 				></Button>
 			</View>
-			<View className=" max-w-md">
-				<Text className="self-center text-3xl m-5 tracking-widest">
-					Coming Soon!
-				</Text>
-			</View>
+			{loading ? (
+				<>
+					<ActivityIndicator
+						size="large"
+						color="#0000ff"
+					/>
+				</>
+			) : (
+				<View className="max-w-md">
+					<ScrollView>
+						<Table
+							borderStyle={{ borderWidth: 1, borderColor: "#c8e1ff" }}
+						>
+							<Row
+								data={tableHead}
+								className="text-gray-700 uppercase dark:text-gray-400"
+							/>
+						</Table>
+						<Table className="">
+							{intakeHistory.map((intake) => {
+								return (
+									<Row
+										key={intake[0]}
+										data={[
+											intake[0],
+											intake[1],
+											intake[2],
+											intake[3],
+										]}
+										className="text-gray-700 uppercase dark:text-gray-400"
+									/>
+								);
+							})}
+						</Table>
+					</ScrollView>
+				</View>
+			)}
 		</View>
 	);
 };
